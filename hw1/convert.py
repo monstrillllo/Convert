e7b2814aee23dfbbda87ffe54e4af908f633aa70
monstrillllo <evgeniy27112000@gmail.com>
2021-09-29 23:59:08 +0300

    doc added

--- hw1/convert.py
+++ hw1/convert.py
@@ -1,11 +1,17 @@
 import argparse
-import sys
 import pandas as pd
 import pyarrow
 import pyarrow.parquet
 
 
 def read_args():
+    """
+    Parse command line params and depending on result call functions
+
+    Return
+    ------
+    None
+    """
     parser = argparse.ArgumentParser('Convert csv to parquet or parquet to csv')
     parser.add_argument('--csv2parquet', help='convert csv to parquet', type=str, metavar='PATH', nargs=2)
     parser.add_argument('--parquet2csv', help='convert parquet to csv', type=str, metavar='PATH', nargs=2)
@@ -18,7 +24,23 @@ def read_args():
     elif args.schema:
         print(get_schema(args.schema[0]))
 
-def convert_csv_to_parquet(src_path, dst_path):
+
+def convert_csv_to_parquet(src_path: str, dst_path: str) -> None:
+    """
+    Convert source csv file to destination parquet file
+
+    Parameters
+    ----------
+    src_path: str, required
+        path to source file
+    dst_path: str, required
+        path to destination file
+
+    Return
+    ------
+    None
+    """
+
     try:
         parquet = pd.read_csv(src_path)
         parquet.to_parquet(dst_path)
@@ -26,7 +48,22 @@ def convert_csv_to_parquet(src_path, dst_path):
     except Exception as ex:
         print(ex)
 
-def convert_parquet_to_csv(src_path, dst_path):
+
+def convert_parquet_to_csv(src_path: str, dst_path: str) -> None:
+    """
+    Convert source parquet file to destination csv file
+
+    Parameters
+    ----------
+    src_path: str, required
+        path to source file
+    dst_path: str, required
+        path to destination file
+
+    Return
+    ------
+    None
+    """
     try:
         csv_ = pd.read_parquet(src_path, engine='pyarrow')
         csv_.to_csv(dst_path, index=False)
@@ -34,16 +71,29 @@ def convert_parquet_to_csv(src_path, dst_path):
     except Exception as ex:
         print(ex)
 
-def get_schema(path):
+
+def get_schema(path: str) -> pyarrow.Schema:
+    """
+    Convert source parquet file to destination csv file
+
+    Parameters
+    ----------
+    path: str, required
+        path to file
+
+    Return
+    ------
+    None
+    """
     try:
-        parquet_file_schema = pyarrow.parquet.read_schema(path)
-        return parquet_file_schema
+        return pyarrow.parquet.read_schema(path)
     except Exception as ex:
-        return ex
+        print(ex)
+
 
 def main():
     read_args()
 
+
 if __name__ == '__main__':
-    # print(sys.argv)
     main()
